6074f0d66a4221ad37f9745cc5f1ddcbadd94266
Naveen Kumar <connect.naveenk@gmail.com>
2024-09-02 14:53:12 +0530

    ""

--- a.ts
+++ a.ts
@@ -1,10 +1,18 @@
-const a:number = 1;
-console.log(a);
+const x: number = 1;
+console.log(x);
 
 
-function greet(firstName: string) {
+function greet(firstName: string, lastName: string, age: number) {
   console.log("hello" + firstName);
-  
+
 }
 
-greet("ram");
+greet("ram", "raj", 22);
+
+const sum = (a:number, b:number) : number =>{
+
+    return a+b;
+}
+
+const value = sum(4,8);
+console.log(value);
